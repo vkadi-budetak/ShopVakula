8f9ecdb9db20850260a82106cc692b82da6c7ce4
Vladyslav Kravchenko <alialiqtu@gmail.com>
2026-01-08 21:36:54 +0100

    add cart

--- js/cart.js
+++ js/cart.js
@@ -1,5 +1,27 @@
+const productList = document.getElementById("product-container");
+
+productList.addEventListener("click", (e) => {
+  if (e.target.classList.contains("buy-btn")) {
+    const id = e.target.dataset.id;
+    addToCart(id);
+  }
+});
+
 let cart = [];
 
 let addToCart = (productId) => {
-    console.log("Додаємо товар з ID: " + productId)
-}
+  const product = window.allProducts.find(
+    (item) => item.id === parseInt(productId)
+  );
+
+  if (product) {
+    cart.push(product);
+
+    console.log("Поточний кошик:", cart);
+  }
+
+  const countSpan = document.querySelector(".cart-count");
+  if (countSpan) {
+    countSpan.textContent = cart.length;
+  }
+};
